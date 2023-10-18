242882fa44d9df1ec9349e05d00f09dbf27c66b9
mehdi <mehdi.aissi@outlook.fr>
2023-10-18 08:18:51 +0200

    setting up'

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module edt.sdialprojet {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens edt.sdialprojet to javafx.fxml;
+    exports edt.sdialprojet;
+}
